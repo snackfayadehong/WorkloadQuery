2832378202619451afefa22789e567825745148f
fayadehong <snackchina@163.com>
2024-02-27 16:26:59 +0800

    调整程序初始化逻辑，先读取配置文件、初始化日志，再初始化数据库连接

--- main/Workload.go
+++ main/Workload.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"WorkloadQuery/conf"
 	clientDb "WorkloadQuery/db"
 	"WorkloadQuery/logger"
 	"WorkloadQuery/middleware"
@@ -29,7 +30,7 @@ func main() {
 			v1.POST("/change_prod", service.ChangeProductInfoService)
 		}
 	}
-	err := r.Run(fmt.Sprintf("%s:%s", clientDb.Configs.Server.IP, clientDb.Configs.Server.Port))
+	err := r.Run(fmt.Sprintf("%s:%s", conf.Configs.Server.IP, conf.Configs.Server.Port))
 	if err != nil {
 		zap.L().Error("ERROR", zap.Error(err))
 		return
@@ -42,7 +43,7 @@ func Cors() gin.HandlerFunc {
 		// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 		// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST")
 		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Content-Length")
-		if clientDb.Configs.Server.RunModel == "debug" {
+		if conf.Configs.Server.RunModel == "debug" {
 			c.Header("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 		} else {
 			c.Header("Access-Control-Allow-Origin", "http://172.21.1.158:5173")
@@ -60,13 +61,17 @@ func Cors() gin.HandlerFunc {
 
 // 初始化程序
 func init() {
-	err := clientDb.Init()
+	// 读取配置文件
+	err := conf.InitSetting()
 	if err != nil {
 		panic(err)
 	}
 	err = logger.InitLog()
 	if err != nil {
-		zap.L().Error("ERROR:", zap.Error(err))
 		return
+	}
+	err = clientDb.Init()
+	if err != nil {
+		panic(err)
 	}
 }
