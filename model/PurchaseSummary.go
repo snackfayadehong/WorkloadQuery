ba83f88277bea895be537dfd59628dc9dc403713
fayadehong <snackchina@163.com>
2024-08-30 09:44:01 +0800

    未到货订单报错bug修复

--- model/PurchaseSummary.go
+++ model/PurchaseSummary.go
@@ -34,5 +34,5 @@ type PurchaseSummaryDetail struct {
 
 type AllPurchaseSummary struct {
 	PurchaseSummary
-	Children *[]PurchaseSummaryDetail `json:"children"`
+	Children *[]PurchaseSummaryDetail `gorm:"_" json:"children"`
 }
