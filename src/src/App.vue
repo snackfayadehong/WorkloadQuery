29b3909dde22872cb0681ba4b9f4682c05b71b95
fayadehong <1241092760@qq.com>
2025-12-19 23:53:12 +0800

    import zhcn

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-    <router-view />
+    <el-config-provider :locale="zhCn">
+        <router-view />
+    </el-config-provider>
 </template>
 
-<script>
-export default {
-    name: "App"
-};
+<script setup>
+import { zhCn } from 'element-plus/es/locale/index' // 引入中文语言包
+
 </script>
+
+<style>
+/* 全局样式可以放在这里 */
+</style>
